ad5ba4d71d76984ffb563a30c9dd8f9852643f36
GeorgeLNicola <georgios.nicola@hotmail.com>
2021-03-23 11:56:24 +0000

    "Fixed Margins and Map"

---
+++ europe.js
@@ -0,0 +1,132 @@
+    // pos is position of where the user in the test or which question they're up to
+    var pos = 0, test, test_status, question, choice, choices, chA, chB, chC, correct = 0;
+    // this is a multidimensional array with 4 inner array elements with 5 elements inside them
+    var questions = [
+      {
+        question: "Which country in the Mediterranean speaks both Greek and Turkish? Hint: it borders Asia and Africa", 
+            a: "Sicily",
+            b: "Cyprus",
+            c: "Malta",
+            answer: "B"
+},
+    {
+        question: "What European country has the strongest army in the continent?",
+            a: "Switzerland",
+            b: "Italy",
+            c: "Russia",
+            answer: "C"
+},
+
+    {
+        question: "Which European country is known for its history of trolls, harsh snow filled winters, fjords and mountains?",
+            a: "Romania",
+            b: "Norway",
+            c: "Iceland",
+            answer: "B"
+},
+
+    {
+        question: "Which African country has the largest GDP and Economy?", 
+            a: "Nigeria",
+            b: "Ghana",
+            c: "Chad",
+            answer: "A"
+    },
+
+    {
+        question: "Which African country has the most software engineers?",
+            a: "Nigeria",
+            b: "Ghana",
+            c: "Somalia",
+            answer: "A"
+    },
+
+    {
+        question: "Which African country is geographically connected to the Middle East?",
+            a: "Nigeria",
+            b: "Egypt",
+            c: "Chad",
+            answer: "B"
+        },
+    {
+        question: "Which country has the most oppurtunity in relation to jobs in the technology field?",
+            a: "USA",
+            b: "Sweden",
+            c: "Canada",
+            answer: "A"
+        },
+    {
+        question: "Which country is the largest in North America?",
+            a: "USA",
+            b: "Canada",
+            c: "Mexico",
+            answer: "B"
+        },
+    {   question: "Which city in the UK has the highest population?",
+            a: "Edinburgh",
+            b: "Glasgow",
+            c: "London",
+            answer: "C"
+        
+        },
+    {
+        question: "Which city is the USA is known for a plethora of tech jobs and great food?",
+            a: "Austin, Texas",
+            b: "Cleveland, Ohio",
+            c: "Denver, Colorado",
+            answer: "A"
+        }
+            
+      ];
+    // this get function is short for the getElementById function  
+    function get(x){
+      return document.getElementById(x);
+    }
+    // this function renders a question for display on the page
+    function renderQuestion(){
+      test = get("test");
+      if(pos >= questions.length){
+        test.innerHTML = "<h2>You got "+correct+" of "+questions.length+" questions correct</h2>";
+        get("test_status").innerHTML = "Test completed";
+        // resets the variable to allow users to restart the test
+        pos = 0;
+        correct = 0;
+        // stops rest of renderQuestion function running when test is completed
+        return false;
+      }
+      get("test_status").innerHTML = "Question "+(pos+1)+" of "+questions.length;
+      
+      question = questions[pos].question;
+      chA = questions[pos].a;
+      chB = questions[pos].b;
+      chC = questions[pos].c;
+      // display the question
+      test.innerHTML = "<h3>"+question+"</h3>";
+      // display the answer options
+      // the += appends to the data we started on the line above
+      test.innerHTML += "<label> <input type='radio' name='choices' value='A'> "+chA+"</label><br>";
+      test.innerHTML += "<label> <input type='radio' name='choices' value='B'> "+chB+"</label><br>";
+      test.innerHTML += "<label> <input type='radio' name='choices' value='C'> "+chC+"</label><br><br>";
+      test.innerHTML += "<button id='submitbtn' onclick='checkAnswer()'>Submit Answer</button>";
+    }
+    function checkAnswer(){
+      // use getElementsByName because we have an array which it will loop through
+      choices = document.getElementsByName("choices");
+      for(var i=0; i<choices.length; i++){
+        if(choices[i].checked){
+          choice = choices[i].value;
+        }
+      }
+      // checks if answer matches the correct choice
+      if(choice == questions[pos].answer){
+        //each time there is a correct answer this value increases
+        correct++;
+      }
+      // changes position of which character user is on
+      pos++;
+      // then the renderQuestion function runs again to go to next question
+      renderQuestion();
+    }
+    // Add event listener to call renderQuestion on page load event
+    window.addEventListener("load", renderQuestion);
+
